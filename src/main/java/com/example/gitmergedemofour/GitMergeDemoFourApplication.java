be3e50ae23c4224ebe4d15e183684302ab28b6af
王钰 <wangyu9@bonc.com.cn>
2022-01-16 23:57:37 +0800

    master:提交远程master分支

---
+++ src/main/java/com/example/gitmergedemofour/GitMergeDemoFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemofour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoFourApplication.class, args);
+    }
+
+}
